50c6fd1fee5ac3fda139e595e70cbb888643e4b5
Paweł <pawellinek3d@gmail.com>
2024-02-16 15:04:55 +0100

    build(app): sitemap modification

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 
 //for now static later when we have dynamic content dynamic
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "/";
+  const baseUrl = "https://notbyte.com";
   return [
     {
       url: baseUrl,
