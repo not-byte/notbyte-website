562d9d1ae2e59e506113bc419f78c96b0b08a956
Paweł <pawellinek3d@gmail.com>
2024-02-25 00:07:51 +0100

    fix(app): fixed naming issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import { Chakra_Petch } from 'next/font/google'
+import { Chakra_Petch } from "next/font/google";
 
 const chakra = Chakra_Petch({
-  weight: ['700'],
-  subsets: ['latin'],
-  display: 'swap'
-})
+  weight: ["700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-[100vh] bg-white dark:bg-night">
-      <h1 className={`${chakra.className} bg-colors text-transparent bg-clip-text text-[12rem] lowercase -translate-y-[20%]`}>
-        Coming soon_
+      <h1
+        className={`${chakra.className} bg-colors text-transparent bg-clip-text text-[12rem] lowercase -translate-y-[20%]`}
+      >
+        Coming Soon_
       </h1>
     </main>
   );
