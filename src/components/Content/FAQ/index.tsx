9bc113ed0b7960bdd36a0010b913d3636f010564
Paweł <pawellinek3d@gmail.com>
2024-06-05 21:45:37 +0200

    backstage: misc responsive fixes

--- src/components/Content/FAQ/index.tsx
+++ src/components/Content/FAQ/index.tsx
@@ -3,7 +3,7 @@ import { FAQItem } from "./FAQItem";
 
 const FaqSection = () => {
   return (
-    <div className="p-6 space-y-6 w-full flex justify-center flex-col items-center overflow-hidden mt-[30vh]">
+    <div className="p-6 space-y-6 wqhd:w-[60vw] xl:w-[70vw] md:w-[75vw] sm:w-[90vw] flex justify-center flex-col items-center overflow-hidden mt-[30vh]">
       <h2 className="text-5xl wqhd:text-7xl text-black dark:text-white mb-20 font-semibold">
         FAQ
       </h2>
