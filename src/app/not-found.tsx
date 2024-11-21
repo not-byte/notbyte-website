484c9f74031595fd3a6d85084b5374ff67f159eb
Braspi <shutmer@gmail.com>
2024-11-21 21:17:11 +0100

    feat(app): small fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,10 +5,10 @@ const NotFound = () => {
     <div className="h-screen flex gap-4 flex-col justify-center items-center text-white">
       <h1 className="text-8xl font-bold animate-pulse">404</h1>
       <p className="text-2xl">Oops! Page not found.</p>
-      <p className="text-md text-gray-400">
+      <p className="text-sm md:text-md text-gray-400">
               The page you are looking for doesn’t exist or has been moved.
       </p>
-      <a href="/" className="text-lg space-y-4 bg-colors hover:brightness-75 text-white pl-10 pr-16 py-2 transition-[filter] duration-300 corner-br corner-black">Come back</a>
+      <a href="/" className="text-lg space-y-4 bg-colors hover:brightness-75 text-white pl-10 pr-16 py-2 transition-[filter] duration-300 corner-br">Come back</a>
     </div>
   );
 };
