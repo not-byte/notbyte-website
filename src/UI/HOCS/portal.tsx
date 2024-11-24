14ba6fefe05c1353d5c756fd297b55f95b6a8144
Braspi <shutmer@gmail.com>
2024-11-24 18:34:43 +0100

    fix(app): fuck eslint 23123

--- src/UI/HOCS/portal.tsx
+++ src/UI/HOCS/portal.tsx
@@ -1,41 +1,41 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, ForwardedRef, ComponentType } from "react";
 import { createPortal } from "react-dom";
 import { memo, forwardRef } from "react";
 
 interface PortalProps {
-  portalId: string;
+    portalId: string;
 }
 
 function withPortal<P extends object>(
-  WrappedComponent: React.ComponentType<P>,
+  WrappedComponent: ComponentType<P>,
   portalProps: PortalProps
 ) {
-  // @ts-ignore
-  return memo(
-    forwardRef(function PortalWrapper(props: P, ref: React.ForwardedRef<any>) {
-      const { portalId } = portalProps;
-      const [mounted, setMounted] = useState<boolean>(false);
-      const portalContainerRef = useRef<Element | null>(null);
+  // eslint-disable-next-line react/display-name
+  const PortalWrapper = forwardRef<unknown, P>((props, ref) => {
+    const { portalId } = portalProps;
+    const [mounted, setMounted] = useState<boolean>(false);
+    const portalContainerRef = useRef<Element | null>(null);
 
-      useEffect(() => {
-        setMounted(true);
-        portalContainerRef.current =
-          document.querySelector<HTMLElement>(portalId);
-        return () => {
-          portalContainerRef.current = null;
-        };
-      }, [portalId]);
+    useEffect(() => {
+      setMounted(true);
+      portalContainerRef.current = document.querySelector<HTMLElement>(portalId);
+      return () => {
+        portalContainerRef.current = null;
+      };
+    }, [portalId]);
 
-      if (!portalContainerRef.current || !mounted) {
-        return null;
-      }
+    if (!portalContainerRef.current || !mounted) {
+      return null;
+    }
 
-      return createPortal(
-        <WrappedComponent {...props} ref={ref} />,
-        portalContainerRef.current
-      );
-    })
-  );
+    return createPortal(
+      <WrappedComponent {...props} ref={ref} />,
+      portalContainerRef.current
+    );
+  });
+
+  // Wracamy z memo, aby optymalizować renderowanie
+  return memo(PortalWrapper);
 }
 
 export default withPortal;
